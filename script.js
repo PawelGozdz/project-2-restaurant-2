c1f48c9454793e4aa4c0d592650cf98fb1ee5cc7
Pawel Gozdz <pawel.gozdz.web.dev@gmail.com>
2018-04-22 19:29:43 +0200

    Finished project

---
+++ script.js
@@ -0,0 +1,31 @@
+var slideIndex = 0;
+
+changeBg();
+changeSlide();
+
+function changeSlide() {
+   var textSlider = document.querySelectorAll('.text-slider');
+   
+   Array.from(textSlider).forEach(function(item) {
+      item.style.display = 'none';
+   });
+
+   textSlider[slideIndex].style.display = 'block';
+   slideIndex++;
+   if(slideIndex >= textSlider.length) {slideIndex = 0;}
+
+  setTimeout(changeSlide, 4000);
+
+}
+
+function changeBg() {
+   var bg = document.querySelector('body');
+   var bgIndex = Math.floor(Math.random() * 5 ) + 1;
+   
+   if(bgIndex >= 6) {bgIndex = 1;}
+   bg.style.backgroundImage = 'url(img/bg-' + bgIndex + '.jpg)';
+   bgIndex++;
+   setTimeout(changeBg, 7500);
+}
+
+
